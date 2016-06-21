e7d5b2669f8a21544aa00196a99088e18f40471f
nonylene <cookie.speaker@gmail.com>
2016-06-22 00:08:36 +0900

    set database and nullable

--- face.py
+++ face.py
@@ -1,12 +1,18 @@
 import peewee
 
+from os import path
+
+db = peewee.SqliteDatabase(path.join(path.abspath(path.dirname(__file__)), 'main.db'))
+
 class Face(peewee.Model):
-    id = peewee.IntegerField(primary_key = True)
     story = peewee.IntegerField(null = False)
     pic = peewee.IntegerField(null = False)
     x = peewee.IntegerField(null = False)
     y = peewee.IntegerField(null = False)
     w = peewee.IntegerField(null = False)
     h = peewee.IntegerField(null = False)
-    chalacter = peewee.TextField()
-    path = peewee.TextField()
+    character = peewee.TextField(null = True)
+    path = peewee.TextField(null = False)
+
+    class Meta:
+        database = db
