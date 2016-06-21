f5a6b2b4d3b5f1886a512c6dd132e0c0ddb47fd1
nonylene <cookie.speaker@gmail.com>
2016-06-22 01:14:26 +0900

    fix layout

--- templates/main.js
+++ templates/main.js
@@ -1,20 +1,4 @@
-function setVal(obj){
-	var id = $(obj).attr("id");
-	if($(obj).hasClass('selected')){
-		$(obj).removeClass('selected');
-		$("#"+id+"_input").val('');
-	}else{
-		$(obj).addClass('selected');
-		$("#"+id+"_input").val(id);
-	}
-};
-
 $(function(){
-	$('input').focus(function(){
-		$(this).parent().addClass('focus');
-	}).blur(function(){
-		$(this).parent().removeClass('focus');
-	});
 	$(document).bind('keydown', 'ctrl+return', function(){
 		$('#submit').click();
 	});
